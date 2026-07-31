fbdd91d8a4a63555a31215eb0c2a52e5facb6cf4
verdesotostiven4-commits <verdesotostiven4@gmail.com>
2026-07-30 22:19:41 -0600

    Use local ZIP implementation

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      fflate: new URL("./src/fflate-local.ts", import.meta.url).pathname,
+    },
+  },
 });
